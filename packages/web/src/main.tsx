3ebb54d563bdeb956b21d2900a68c1e3a1a098cb
nea <git@nea.dev>
2026-06-10 21:39:09 -0400

    Update public sharing view

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "katex/dist/katex.min.css";
-import "highlight.js/styles/github.css";
 import "./styles.css";
 import { App } from "./App";
 
